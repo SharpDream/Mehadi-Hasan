b03003613bffaec69550818c5203c7583f75ea76
ShapDream <144349058+SharpDream@users.noreply.github.com>
2024-06-09 10:54:58 +0600

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Mahedi Hasan</span> a student at Safiuddin Sarker Academy and College in Class 9. I enjoy exploring various interests such as art, food, travel, science, and math. With a curious mind and a passion for learning, I find joy in unraveling the mysteries of the world around me."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
